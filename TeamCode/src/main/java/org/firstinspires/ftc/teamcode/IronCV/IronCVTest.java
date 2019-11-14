cd0841e7a8b584a0a5934a6ed75033bf2d2d2a4a
flashman2000 <vedant.kt@gmail.com>
2019-11-14 00:51:40 -0600

    Installed OpenRC-Turbo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronCV/IronCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronCV/IronCVTest.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.IronCV;
 
-import com.disnodeteam.dogecv.math.Line;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
